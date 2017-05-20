f0e12001dcd9f51d146165ffe68fe0d7f56d341d
Marcel Fahle <m.fahle@gmail.com>
2017-05-20 13:39:26 +0200

    adds basic play and reload actins to new player

--- src/components/AudiclePlayer.js
+++ src/components/AudiclePlayer.js
@@ -6,33 +6,56 @@ class AudiclePlayer extends React.Component {
     super( props );
 
     this.state = {
-      audio: null
+      play: false
     }
 
 		this.playAudio = this.playAudio.bind(this);
   }
 
   componentDidMount() {
-    this.setState({audio: document.getElementById('music')});
+    console.log("player did mount");
   }	
 
-	playAudio() {
-    const { audio } = this.state;
-    if (audio.paused) {
-      audio.play();
+  componentWillReceiveProps( nextProps ) {
+    console.log("will receive", nextProps);
+  }
+
+  componentDidUpdate( prevProps, prevState ) {
+    console.log("did update"); 
+    if ( !this.player ) { return; }
+
+    const { src } = this.props;
+
+    if (src) {
+      if (prevProps.src !== this.props.src) {
+        this.player.load();
+      }
+      this.player.play();
     } else {
-      audio.pause();
+      this.player.pause();
     }
+
+  }
+
+	playAudio( e ) {
+    console.log(e, this.player);
+    e.stopPropagation();
+    this.player.play();
 	}
 
   render() {
     return (
       <div>
-       	<audio id="music" autoPlay preload="true">
-					<source src={this.props.currentFile} />
+        <audio 
+          ref={ (player) => { this.player = player } }
+          autoPlay 
+          preload="true">
+					<source src={this.props.src} />
 				</audio>
 
 				<button id="pButton" className="play" onClick={this.playAudio}>CLICK ME</button>	
+
+        <p>{ this.props.src }</p>
 
       </div>
     )
